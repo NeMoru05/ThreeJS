de61c81ace9f66c2facaa138d73d8830160d7a95
SleepyMoru <87416908+NeMoru05@users.noreply.github.com>
2023-08-15 11:33:33 +0800

    Model Test Upload

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(   window.innerWidth,
-                    window.innerHeight );
+renderer.setSize(   window.innerWidth/1,
+                    window.innerHeight/1 );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 3;
 
 function animate() {
 	requestAnimationFrame( animate );
