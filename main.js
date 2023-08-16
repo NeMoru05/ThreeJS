fff22fc39550e7aeb48a0d50cf1e9482fb8fbabc
SleepyMoru <87416908+NeMoru05@users.noreply.github.com>
2023-08-16 09:27:23 +0800

    Add Controller

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js' //Loader
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { ArcballControls } from 'three/addons/controls/ArcballControls.js'; // Correct the import path
+
+// Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -7,7 +10,10 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Set up the camera position
-camera.position.z = 5; // Move the camera back along the Z-axis
+camera.position.z = 5;
+
+// Initialize ArchballControls
+const archBallControls = new ArcballControls(camera, renderer.domElement); // Correct the class name
 
 // Load the glTF model using the GLTFLoader
 const loader = new GLTFLoader();
@@ -23,9 +29,14 @@ loader.load('denah-clinik.glb', (gltf) => {
     directionalLight.position.set(1, 1, 1);
     scene.add(directionalLight);
 
-    // Start rendering the scene
+    // Start rendering the scene with controls
     const animate = () => {
         requestAnimationFrame(animate);
+
+        // Update ArchBall controls
+        archBallControls.update();
+
+        // Render the scene
         renderer.render(scene, camera);
     };
     animate();
